fe277540b04c47ef54207e8084a61c41968f590d
romamsiligali <romamsiligali@gmail.com>
2026-03-12 12:17:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // ===== CONFIGURATION =====
 // Replace this URL with your Google Apps Script Web App URL
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwWKOqo3EyYeNJEd4v_74xy-SZSf6gxBshpXn9NTza7fMkC5SUL71Vm4iKlpXONhos4gg/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxR0CbKZaaQP9LBkzwUi6hjYoYzA6pniRL-0YuQfA8QvJcI0bqD-zSH8c9oGK8PiDBhLg/exec';
 
 // ===== INITIALIZE =====
 document.addEventListener('DOMContentLoaded', function() {
